cab461a39f80b0850c11d4488609a95ce7653987
ozatalcs <ozatalcs@rose-hulman.edu>
2019-01-31 20:30:31 -0500

    Conner Ozatalar Exam 3

--- src/problem2.py
+++ src/problem2.py
@@ -74,7 +74,7 @@ def shape(n):
     :type n: int
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     ####################################################################
     # IMPORTANT: In your final solution for this problem,
--- src/problem5.py
+++ src/problem5.py
@@ -3,9 +3,9 @@ Exam 3, problem 5.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Conner Ozatalar.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the   is_prime   function defined below.
+# Done: 2.  READ the doc-string for the   is_prime   function defined below.
 # It is the same as you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -145,11 +145,18 @@ def problem5(seq_of_seq):
       :rtype: (list of int) | int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
-
+    returned_list = []
+    for j in range(len(seq_of_seq)):
+        if len(seq_of_seq[j]) > 0:
+            largest_int = seq_of_seq[j][0]
+            for k in range(len(seq_of_seq[j])):
+                if seq_of_seq[j][k] > largest_int:
+                    largest_int = seq_of_seq[j][k]
+            returned_list = returned_list + [largest_int]
+    return returned_list
 ###############################################################################
 # Our tests use the following to print error messages in red.
 # Do NOT change it.  You do NOT have to do anything with it.
